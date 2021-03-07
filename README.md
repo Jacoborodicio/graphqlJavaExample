59aa106b95010cfb34787b3afbdf7ba929606401
Jacobo <jacoborodicio@gmail.com>
2021-03-07 13:29:33 +0100

    feat: Example of GraphqlInputType updating actor address & added README.md with some example queries for /graphiql

---
+++ README.md
@@ -0,0 +1,60 @@
+Examples of queries used in graphiql:
+```
+# query ($id:Int){
+#   getAllActors {
+#     actorId
+#     firstName
+#     lastName
+#     CUSTOM_KEY:address,
+#     film {
+#       name
+#     }
+#   }
+  
+#   getActorById(id:$id) {
+#     actorId
+#     firstName
+#     lastName
+#     address
+#     film{
+#       name
+#       launchYear
+#     }
+#   }
+# }
+
+# Updating the address of the user with a certern id
+# mutation ($id:Int) {
+#   updateAddress(id:$id, address:"Ostuzzistrasse 2") {
+#     firstName
+#     lastName
+#     address
+#   }
+# }
+
+# # Showing Film data depending on a variable & showing the use of fragments
+# query ($withFilm:Boolean!, $id:Int) {
+#   getActorById(id:$id) {
+#     ...actorDetails
+#     dob,
+#     film @include(if:$withFilm) {
+#       name,
+#       launchYear
+#     }
+#   }
+# }
+
+# Example updating with object
+mutation ($input:AddressInput){
+  updateAddressByObjectType(input:$input) {
+    ...actorDetails
+  }
+}
+
+# Fragment to spread some common used keys
+fragment actorDetails on Actor {
+  firstName
+  lastName
+  address
+}
+```
--- src/main/java/com/jr/service/ActorService.java
+++ src/main/java/com/jr/service/ActorService.java
@@ -1,16 +1,19 @@
 package com.jr.service;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.jr.model.Actor;
+import com.jr.model.AddressInput;
 import com.jr.repository.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ActorService implements GraphQLQueryResolver {
+public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ActorRepository repository;
@@ -22,5 +25,21 @@ public class ActorService implements GraphQLQueryResolver {
     public Actor getActorById(Integer id) {
         Optional<Actor> actor = repository.findById(id);
         return actor.orElseGet(actor::get);
+    }
+
+    @Transactional
+    public Actor updateAddress(Integer id, String address) {
+        Actor actor = repository.findById(id).get();
+        actor.setAddress(address);
+        repository.save(actor);
+        return actor;
+    }
+
+    @Transactional
+    public Actor updateAddressByObjectType(AddressInput input) {
+        Actor actor = repository.findById(input.getActorId()).get();
+        actor.setAddress(input.getAddress());
+        repository.save(actor);
+        return actor;
     }
 }
