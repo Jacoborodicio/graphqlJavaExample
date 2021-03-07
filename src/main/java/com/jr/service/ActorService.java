49490d26b6612823a66aa07c0e0d41e24ce25ff4
Jacobo <jacoborodicio@gmail.com>
2021-03-07 13:50:32 +0100

    WIP: Starting with actor subscribers

--- src/main/java/com/jr/service/ActorService.java
+++ src/main/java/com/jr/service/ActorService.java
@@ -5,18 +5,24 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.jr.model.Actor;
 import com.jr.model.AddressInput;
 import com.jr.repository.ActorRepository;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ActorRepository repository;
+
+    private ConcurrentHashMap<Integer, FluxSink<Actor>> subscribers = new ConcurrentHashMap<>();
 
     public List<Actor> getAllActors() {
         return repository.findAll();
@@ -40,6 +46,14 @@ public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolv
         Actor actor = repository.findById(input.getActorId()).get();
         actor.setAddress(input.getAddress());
         repository.save(actor);
+
+        if (subscribers.get(input.getActorId()) != null) {
+            subscribers.get(input.getActorId()).next(actor);
+        }
         return actor;
+    }
+
+    public Publisher<Actor> onActorUpdate(Integer actorId) {
+         return Flux.create(subscriber -> subscribers.put(actorId, subscriber.onDispose(() -> subscribers.remove(actorId, subscriber))), FluxSink.OverflowStrategy.LATEST);
     }
 }
